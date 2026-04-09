9c65ff91bbc66ba8617e1dbcdabb5d244c5d0c5d
bengal865 <69059209+bengal865@users.noreply.github.com>
2026-04-09 08:21:38 -0400

    Create array-examples.js

---
+++ array-examples.js
@@ -0,0 +1,9 @@
+// Abraham Lincoln
+// 20 MAY 20XX
+// JS Array Examples
+
+// Use const to declare (create) your array
+const fruits = ['orange', 'apple', 'kiwi', 'lemon'];
+
+// Array containing quiz scores
+const scores = [99, 91, 85, 73, 88];
