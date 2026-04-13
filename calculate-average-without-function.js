7003756cd12d7019159857c23f38150681679c54
bengal865 <69059209+bengal865@users.noreply.github.com>
2026-04-13 08:17:30 -0400

    Update calculate-average-without-function.js

--- calculate-average-without-function.js
+++ calculate-average-without-function.js
@@ -1,1 +1,28 @@
+// Abraham Lincoln
+// 10 APR 20XX
+// Calculate Average Without Function
 
+// Student 1 (Mike)
+const score1 = 88;
+const score2 = 75;
+const student1NumScores = 2;
+
+const totalPoints1 = score1 + score2;
+const student1Average = totalPoints1 / student1NumScores;
+
+
+const outputString1 = `The average of ${score1} and ${score2} is: ${student1Average}`;
+console.log(outputString1);
+
+// Student 2 (Alyssa)
+const score3 = 92;
+const score4 = 86;
+
+const student2NumScores = 2;
+
+const totalPoints2 = score3 + score4;
+const student2Average = totalPoints2 / student2NumScores;
+
+
+const outputString2 = `The average of ${score3} and ${score4} is: ${student2Average}`;
+console.log(outputString2);
